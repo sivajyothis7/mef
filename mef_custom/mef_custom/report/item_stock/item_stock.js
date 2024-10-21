d76d44f3953a019fb49148f06c2a7f0e2bc48540
sivajyothis7 <sivajyothis9446@gmail.com>
2024-10-21 12:40:31 +0530

    first

--- mef_custom/mef_custom/report/item_stock/item_stock.js
+++ mef_custom/mef_custom/report/item_stock/item_stock.js
@@ -2,17 +2,12 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Item Stock"] = {
-    filters: [
-        {
-            fieldname: "item_code",
-            label: __("Item"),
-            fieldtype: "Link",
-            options: "Item",
-            get_query: function () {
-                return {
-                    query: "mef_custom.report.item_stock.item_stock.get_items_with_main_store_price"
-                };
-            }
-        }
-    ]
+	filters: [
+		{
+			fieldname: "item_code",
+			label: __("Item"),
+			fieldtype: "Link",
+			options: "Item",
+		},
+	],
 };

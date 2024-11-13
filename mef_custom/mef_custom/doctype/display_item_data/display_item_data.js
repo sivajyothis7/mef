0fb05a9c92a59ecea45a63b45bc1a6ef23ad4d77
sivajyothis7 <sivajyothis9446@gmail.com>
2024-11-13 15:34:27 +0530

    first

---
+++ mef_custom/mef_custom/doctype/display_item_data/display_item_data.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, siva and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Display Item Data", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ mef_custom/mef_custom/doctype/display_item_data/test_display_item_data.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, siva and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDisplayItemData(FrappeTestCase):
+	pass
